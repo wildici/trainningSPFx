10865f62d0d0bf640ba1a5f097d1722a3c863086
Wildici <wildici@hotmail.com>
2021-10-26 14:12:15 -0300

    [FEAT] create new webpart for display list items

---
+++ src/webparts/showListItems/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IShowListItemsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ShowListItemsWebPartStrings' {
+  const strings: IShowListItemsWebPartStrings;
+  export = strings;
+}
