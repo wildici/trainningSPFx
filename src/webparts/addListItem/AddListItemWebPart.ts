b37c1df8b47d77429a5e657fb45bfe0eaae79d17
André Luiz <an3dree@gmail.com>
2021-10-26 12:52:13 -0300

    [FEAT] create new webpart for add list items

---
+++ src/webparts/addListItem/AddListItemWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+
+import * as strings from 'AddListItemWebPartStrings';
+import AddListItem from './components/AddListItem';
+import { IAddListItemProps } from './components/IAddListItemProps';
+
+export interface IAddListItemWebPartProps {
+  description: string;
+}
+
+export default class AddListItemWebPart extends BaseClientSideWebPart<IAddListItemWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IAddListItemProps> = React.createElement(
+      AddListItem,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/addListItem/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAddListItemWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'AddListItemWebPartStrings' {
+  const strings: IAddListItemWebPartStrings;
+  export = strings;
+}
